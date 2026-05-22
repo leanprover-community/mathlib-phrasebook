abb6b6f242ea5b748ccfb8ce19312f8eb67e3b54
Damiano Testa <adomani@gmail.com>
2026-05-22 23:14:18 +0100

    remove comments

--- Phrasebook/Template.lean
+++ Phrasebook/Template.lean
@@ -8,14 +8,8 @@ import VersoManual
 import Phrasebook.Meta.Lean
 import Mathlib
 
--- This gets access to most of the manual genre (which is also useful for textbooks)
 open Verso.Genre Manual
-
--- This gets access to Lean code that's in code blocks, elaborated in the same process and
--- environment as Verso
--- Write a code block with ```savedLean ... ``` to save it to an external file.
 open Verso.Genre.Manual.InlineLean
-
 
 open Phrasebook
 
